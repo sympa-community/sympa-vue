5bad5700c0447d934f9fb4d02fb58e1c602f3be0
Quentin Gliech <quentingliech@gmail.com>
2017-04-02 23:26:13 +0200

    Sidebar tweaks (logout button, style, …)

--- src/store/auth/getters.js
+++ src/store/auth/getters.js
@@ -1,6 +1,9 @@
+import gravatar from 'gravatar';
+
 export const errored = state => state.error !== null;
 export const errorMessage = state => state.error && state.error.message;
 export const pending = state => state.pending;
 export const logged = state => state.user !== null;
 export const fullname = state => (state.user ? `${state.user.name.first} ${state.user.name.last}` : '');
 export const email = state => state.user && state.user.email;
+export const picture = state => state.user && gravatar.url(state.user.email, { s: 128 });
