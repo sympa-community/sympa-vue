115c9554a1c07a43d85676ab719327a83dc11697
ludovicm67 <ludovicmuller1@gmail.com>
2017-04-03 11:47:29 +0200

    start for the GridItem

---
+++ src/components/layout/GridItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid-item">
+    <slot>
+      <img src="//placehold.it/500x500" alt="placeholder">
+      <div class="content">
+        <p class="title">Random</p>
+        <p>random@example.com</p>
+      </div>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'grid-item',
+};
+</script>
+
+<style>
+.grid-item > img {
+  height: 172px;
+  width: 172px;
+}
+</style>
--- src/components/pages/Discussions.vue
+++ src/components/pages/Discussions.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>Discussions</h1>
+  <div>
+    <h1>Discussions</h1>
+    <grid-item></grid-item>
+  </div>
 </template>
 
 <script>
+import GridItem from '@/components/layout/GridItem.vue';
+
 export default {
   name: 'discussions',
+  components: {
+    GridItem,
+  },
 };
 </script>
