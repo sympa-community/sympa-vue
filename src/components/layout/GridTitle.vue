b54c06cc928cb0519015a336182fd71b2e6c6899
ludovicm67 <ludovicmuller1@gmail.com>
2017-05-22 19:00:54 +0200

    update Discussion layout

---
+++ src/components/layout/GridTitle.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2 class="grid-title">
+    <slot></slot>
+  </h2>
+</template>
+
+<script>
+export default {
+  name: 'grid-title',
+};
+</script>
+
+<style>
+.grid-title {
+  font-size: 34px;
+  font-weight: 400;
+  margin-top: 60px;
+}
+.grid-title:after {
+  display: block;
+  content: '';
+  margin-top: 20px;
+  background-color: #F1453D;
+  width: 110px;
+  height: 3px;
+}
+@media screen and (max-width: 480px) {
+  .grid-title {
+    display: none;
+  }
+}
+</style>
--- src/components/pages/Discussions.vue
+++ src/components/pages/Discussions.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <h1>Discussions</h1>
+  <div class="discussions">
+    <grid-title>Groupe de discussions</grid-title>
+    <grid-list>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+      <grid-item></grid-item>
+    </grid-list>
+    <grid-title>Titre du groupe</grid-title>
     <grid-list>
       <grid-item></grid-item>
       <grid-item></grid-item>
@@ -15,15 +27,28 @@
   </div>
 </template>
 
+<style scoped>
+.discussions {
+  margin-top: -35px;
+}
+@media screen and (max-width: 480px) {
+  .discussions {
+    margin: -25px;
+  }
+}
+</style>
+
 <script>
 import GridList from '@/components/layout/GridList.vue';
 import GridItem from '@/components/layout/GridItem.vue';
+import GridTitle from '@/components/layout/GridTitle.vue';
 
 export default {
   name: 'discussions',
   components: {
     GridList,
     GridItem,
+    GridTitle,
   },
 };
 </script>
